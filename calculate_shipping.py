52ec1f5058745db80056bffb9bc7f1ee4f2685f3
tseppelin <7509073+tseppelin@users.noreply.github.com>
2022-12-12 13:02:55 +0100

    base
    calculate shipping functon&tests

---
+++ calculate_shipping.py
@@ -0,0 +1,93 @@
+import decimal
+
+
+MINIMAL_PRICE = 400
+
+
+ROUTE_DIST = {
+    'under_30': 200,
+    'over_30': 300,
+    'under_10': 100,
+    'under_2': 50
+}
+
+
+DIMENSIONS_COST = {
+    'large': 200,
+    'small': 100
+}
+
+
+IS_FRAGILE_COST = 300
+
+WORKLOAD_COEF = {
+    'extremely high': 1.6,
+    'high': 1.4,
+    'increased': 1.2,
+    'normal': 1
+}
+
+
+class CalculateShippingError(Exception):
+    pass
+
+
+class InputError(CalculateShippingError):
+    pass
+
+
+class ShippingUnavailableError(CalculateShippingError):
+    pass
+
+
+def calculate_shipping_cost(
+    distance: float, dimensions: str, is_fragile: bool, service_workload: str
+) -> decimal.Decimal:
+    """
+    Args:
+        distance (int): distance from A to B
+        dimensions (str): options: 'S' or 'L' - cargo dimensions
+        is_fragile (bool): fragily of cargo
+        service_workload (str): options: extremely_high, high, increased, normal
+
+    Returns:
+        decimal.Decimal: summary cost of shipping
+    """
+    if service_workload not in WORKLOAD_COEF.keys():
+        raise InputError(
+            f'Available options for workload: {WORKLOAD_COEF.keys()}')
+
+    if dimensions not in DIMENSIONS_COST.keys():
+        raise InputError(
+            f'Available options for dimensions: {DIMENSIONS_COST.keys()}')
+
+    if distance <= 0:
+        raise InputError('Distance should be > 0')
+
+    trip_cost = 0
+
+    if distance <= 2:
+        trip_cost += 50
+    if distance > 2 and distance <= 10:
+        trip_cost += 100
+    if distance > 10 and distance <= 30:
+        trip_cost += 200
+    if distance > 30 and is_fragile:
+        raise ShippingUnavailableError(
+            'Fragile goods cannot be transported over a distance of more than 30 km')
+    if distance > 30:
+        trip_cost += 300
+
+    if dimensions == 'small':
+        trip_cost += DIMENSIONS_COST['small']
+    elif dimensions == 'large':
+        trip_cost += DIMENSIONS_COST['large']
+
+    if is_fragile:
+        trip_cost += 300
+
+    workload_coef = WORKLOAD_COEF[service_workload]
+    trip_cost = trip_cost * workload_coef
+    trip_cost = decimal.Decimal(str(trip_cost))
+    trip_cost = round(trip_cost, 2)
+    return max(trip_cost, MINIMAL_PRICE)
---
+++ test_delivery_cost.py
@@ -0,0 +1,66 @@
+import unittest
+from random import randrange
+
+from parameterized import parameterized
+
+from delivery_cost import (calculate_shipping_cost,
+                           MINIMAL_PRICE, ShippingUnavailableError)
+
+
+class TestShippingCost(unittest.TestCase):
+
+    @parameterized.expand([
+        [2, 'large', True, "extremely high", 880],
+        [5, 'large', True, "extremely high", 960],
+        [10, 'large', False, "extremely high", 480],
+        [15, 'large', False, "extremely high", 640],
+        [30, 'large', False, "extremely high", 640],
+        [31, 'large', False, "extremely high", 800],
+    ])
+    def test_distance(self, distance, dimensions,
+                      is_fragile, service_workload, expected):
+
+        self.assertEqual(calculate_shipping_cost(distance, dimensions,
+                                                 is_fragile, service_workload), expected)
+
+    @parameterized.expand([
+        [randrange(31, 100), 'small', False, 'high', 560],
+        [randrange(31, 100), 'large', False, 'high', 700],
+    ])
+    def test_dimensions(self, distance, dimensions,
+                        is_fragile, service_workload, expected):
+        self.assertEqual(calculate_shipping_cost(distance, dimensions,
+                                                 is_fragile, service_workload), expected)
+
+    @parameterized.expand([
+        [randrange(11, 31), 'large', True, 'normal', 700],
+        [randrange(11, 31), 'large', True, 'increased', 840],
+        [randrange(11, 31), 'large', True, 'high', 980],
+        [randrange(11, 31), 'large', True, 'extremely high', 1120],
+    ])
+    def test_workload(self, distance, dimensions,
+                      is_fragile, service_workload, expected):
+        self.assertEqual(calculate_shipping_cost(distance, dimensions,
+                                                 is_fragile, service_workload), expected)
+
+    @parameterized.expand([
+        [1, 'small', False, 'normal', MINIMAL_PRICE],
+    ])
+    def test_minimal_price(self, distance, dimensions,
+                           is_fragile, service_workload, expected):
+        self.assertEqual(calculate_shipping_cost(distance, dimensions,
+                                                 is_fragile, service_workload), MINIMAL_PRICE)
+
+    def test_calculate_fragile(self):
+        with self.assertRaises(ShippingUnavailableError):
+            calculate_shipping_cost(distance=31, dimensions='small',
+                                    is_fragile=True, service_workload='normal')
+
+    @parameterized.expand([
+        [randrange(11, 31), 'large', True, 'high', 980],
+        [randrange(11, 31), 'large', False, 'high', 560],
+    ])
+    def test_calculate_fragile(self, distance, dimensions,
+                               is_fragile, service_workload, expected):
+        self.assertEqual(calculate_shipping_cost(distance, dimensions,
+                                                 is_fragile, service_workload), expected)
